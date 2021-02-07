0cf8eaead73006943f096ea90134ef40ed05cd77
schoemi <dirk@schoemakers.com>
2021-02-07 14:53:40 +0100

    +1
    Road added

---
+++ Modern City.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Modern City" tilewidth="32" tileheight="32" tilecount="260" columns="20">
+ <image source="../../TileEditor/Modern City/Modern City/_Example.png" width="656" height="432"/>
+</tileset>
